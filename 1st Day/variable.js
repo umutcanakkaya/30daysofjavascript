6af3941f66151bbc2960cdfebe84b15421e20fb4
umutcanakkaya <76808039+umutcanakkaya@users.noreply.github.com>
2023-06-03 19:12:56 +0300

    1st Day of 30 Days Challenge
    This is my first day code of learning Javascript.

---
+++ 1st Day/variable.js
@@ -0,0 +1,14 @@
+let firstName = 'Umutcan'
+let lastName = 'Akkaya'
+let country = 'Turkey'
+let city = 'Istanbul'
+let age = 24
+let isMarried = false
+
+const gravity = 9.81
+const  boilingPoint = 100
+const PI = 3.14
+
+let name = 'Umutcan',
+    job = 'Software Developer',
+    live = 'Turkey'
